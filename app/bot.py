35d2a1103eb370626d3a1bbfea6e27e2a542463d
Andrei Koval <andriiko@gmail.com>
2023-01-27 15:13:01 +0200

    split bot and api logic

--- app/bot.py
+++ app/bot.py
@@ -1,11 +1,30 @@
 from aiogram import Dispatcher, Bot, types
 
-from .config import TOKEN
+from .config import TOKEN, WEBHOOK_URL
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
+async def on_startup():
+    await bot.set_webhook(url=WEBHOOK_URL)
+
+
+async def process_update(update):
+    telegram_update = types.Update(**update)
+    Dispatcher.set_current(dp)
+    Bot.set_current(bot)
+    await dp.process_update(telegram_update)
+
+
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
     await message.answer(f"Hi, {message.from_user.full_name}, chat id {message.from_user.id}")
+
+
+async def send_message(chatid: str, message: str):
+    await bot.send_message(chatid, message) # to get your chatid, start the app and send /start message to bot
+
+
+async def shutdown():
+    await bot.session.close()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from aiogram import types, Dispatcher, Bot
 
-from .bot import dp, bot
-from .config import WEBHOOK_PATH, WEBHOOK_URL, API_PATH
+from . import bot as tgbot
+from .config import WEBHOOK_PATH, API_PATH
 
 
 app = FastAPI()
@@ -10,32 +9,25 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def on_startup():
-    webhook_info = await bot.get_webhook_info()
-    if webhook_info.url != WEBHOOK_URL:
-        await bot.set_webhook(
-            url=WEBHOOK_URL
-        )
+    await tgbot.on_startup()
 
 
-@app.get("/")  # to test ngrok and fastAPI to work
+@app.get("/")  # test ngrok and fastAPI to work
 async def home():
     return {"message": "homepage"}
 
 
-@app.post(API_PATH + "/send_message")  # to send message to chat
-async def send_message(chatid: str, message: str):
-    await bot.send_message(chatid, message) # to get your chatid, start the app and send /start message to bot
-    return {"message": "sent success"}
-
-
-@app.post(WEBHOOK_PATH)
+@app.post(WEBHOOK_PATH)  # process events from webhook
 async def bot_webhook(update: dict):
-    telegram_update = types.Update(**update)
-    Dispatcher.set_current(dp)
-    Bot.set_current(bot)
-    await dp.process_update(telegram_update)
+    await tgbot.process_update(update)
+
+
+@app.post(API_PATH + "/send_message")  # send message to chat
+async def send_message(chatid: str, message: str):
+    await tgbot.send_message(chatid, message) # to get your chatid, start the app and send /start message to bot
+    return {"message": "sent success"}
 
 
 @app.on_event("shutdown")
 async def on_shutdown():
-    await bot.session.close()
+    await tgbot.shutdown()
